7ec66599843223414fd53d8c8150303acd065f92
hristo2612 <hristoapps@gmail.com>
2025-08-25 11:41:25 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Hristo on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
